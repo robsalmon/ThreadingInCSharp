86849e4e8b4c383ce586f23777a82f32440b4569
Robert Salmon <robdavidsalmon@msn.com>
2020-07-11 12:47:34 +0100

    Add project files.

---
+++ ThreadingInCSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ThreadingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteY);          
+            
+            // Kick off a new thread
+            t.Start();                               
+            
+            // running WriteY()
+            
+            Console.Write(t.IsAlive);
+            
+            // Simultaneously, do something on the main thread.
+            
+            for (int i = 0; i < 1000; i++) Console.Write("x");
+            
+            Console.Write(t.IsAlive);
+            
+            Console.ReadLine();
+
+            new Thread(Go).Start();
+            Go();
+            Console.ReadLine();
+
+            ThreadTest tt = new ThreadTest(); // Create a common instance
+
+            new Thread(tt.Go2).Start();
+
+            tt.Go2();
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("y");
+        }
+
+        static void Go()
+        {
+            for (int cycles = 0; cycles < 5; cycles++)
+            {
+                Console.Write('?');
+            }
+        }
+
+    }
+    
+}
---
+++ ThreadingInCSharp/ThreadTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingInCSharp
+{
+    class ThreadTest
+    {
+        bool done; 
+
+       
+
+        public void Go2()
+        {
+            if (!done)
+            {
+                done = true;
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
